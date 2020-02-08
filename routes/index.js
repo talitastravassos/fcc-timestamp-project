6e657b4f5bec6fe7cd4958729a70c95845569b7f
talitast <talitatravassos.tt@gmail.com>
2020-02-08 11:35:20 -0300

    added timestamp for date_string undefined

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,16 @@ router.use(express.json());
 
 router.get("/timestamp/:date_string?", (req, res) => {
   const date = new Date(req.params.date_string);
-  // console.log(req.params.date_string)
+  // console.log(date)
   const response = {
     unix: date.getTime(),
     utc: date
   };
-  if (date === undefined) {
-    res.send(new Date());
+
+  if (req.params.date_string === undefined) {
+    response.unix = new Date().getTime();
+    response.utc = new Date();
+    res.send(response);
   } else {
     res.send(response);
   }
